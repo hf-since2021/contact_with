14e25a8020ce13c263e7285816f40d3e1fc6c999
hf-since2021 <hrk.fj.0911mon@gmail.com>
2022-06-04 21:29:56 +0900

    欠席届登録機能修正／一覧表示機能実装

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,7 @@
 class ContactsController < ApplicationController
 
   def index
+    @contacts = Contact.all
   end
 
   def new
@@ -16,9 +17,25 @@ class ContactsController < ApplicationController
 
   def contact_params
     if params[:contact][:request_type_id] == "1"
-      params.require(:contact).permit(:date, :request_type_id, :absent_type_id, :reason).merge(user_id: current_user.id)
-    elsif params[:contact][:request_type_id] == "2" || params[:contact][:request_type_id] == "3"
-      params.require(:contact).permit(:date, :request_type_id, :in_and_out_time_id, :reason).merge(user_id: current_user.id)
+      params.require(:contact).permit(
+            :date, :request_type_id, :absent_type_id, :reason
+          ).merge(
+            user_id: current_user.id, name: current_user.name, sync_status_id: 1
+          )
+    elsif params[:contact][:request_type_id] == "2"
+      time_id = params[:contact][:in_time_id]
+      params.require(:contact).permit(
+            :date, :request_type_id, :reason
+          ).merge(
+            user_id: current_user.id, name: current_user.name, in_and_out_time_id: time_id, sync_status_id: 1
+          )
+    elsif params[:contact][:request_type_id] == "3"
+      time_id = params[:contact][:out_time_id]
+      params.require(:contact).permit(
+            :date, :request_type_id, :reason
+          ).merge(
+            user_id: current_user.id, name: current_user.name, in_and_out_time_id: time_id, sync_status_id: 1
+          )
     end
   end
 end
